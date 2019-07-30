dfbf6596aae8d4dd7b54f316aa60e55d2699e3c0
Drilon Pacarizi <drilonpa@gmail.com>
2019-07-30 21:17:48 +0200

    Issue Tracker

--- index.js
+++ index.js
@@ -1,132 +1,200 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const mongoose = require('mongoose')
-const morgan = require('morgan');
+const express = require("express");
+const app = express();
+const port = 3000;
+const mongoose = require("mongoose");
+const morgan = require("morgan");
 
-const DB_LOCAL = 'mongodb+srv://issue-tracker:issue-tracker@issue-tracker-tx3vl.mongodb.net/test?retryWrites=true&w=majority';
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(morgan('combined'));
+const DB_LOCAL =
+  "mongodb+srv://issue-tracker:issue-tracker@issue-tracker-tx3vl.mongodb.net/test?retryWrites=true&w=majority";
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("combined"));
 
 //Connecting to our mongodb database
-mongoose.connect(DB_LOCAL)
-    .catch(err => console.log(err))
-    .then(() => {
-        console.log("Connected to DB successfully")
-    })
+mongoose
+  .connect(DB_LOCAL)
+  .catch(err => console.log(err))
+  .then(() => {
+    console.log("Connected to DB successfully");
+  });
 
 // User Routes
-const UserModel = require('./models/user.model');
+const UserModel = require("./models/user.model");
 //Create User
-app.post('/user', (req, res, next) => {
-    let user = new UserModel({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        password: req.body.password
-    })
-    UserModel.create(user, (err, response) => {
-        if(err) {
-            console.log(err);
-            return;
-        }
-        res.json(response);
-    })
-})
+app.post("/user", (req, res, next) => {
+  let user = new UserModel({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    password: req.body.password
+  });
+  UserModel.create(user, (err, response) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    res.json(response);
+  });
+});
 // Get all users
-app.get('/user', (req, res) => {
-    UserModel.find({}, (err, result) => {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        res.json(result);
-    })
-})
+app.get("/user", (req, res) => {
+  UserModel.find({}, (err, result) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    res.json(result);
+  });
+});
 //Delete user by id
-app.delete('/user/:id', (req, res, next) => {
-    const id = req.params.id;
-    UserModel.deleteOne({_id: id}, (err, result) => {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        res.json(result);
-    })
-})
+app.delete("/user/:id", (req, res, next) => {
+  const id = req.params.id;
+  UserModel.deleteOne({ _id: id }, (err, result) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    res.json(result);
+  });
+});
 
 // find user by Id
-app.get('/user/:id', (req, res, next) => {
-    const id = req.params.id;
-    UserModel.findById({_id: id}, (err, result) => {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        res.json(result);
-    })
-})
+app.get("/user/:id", (req, res, next) => {
+  const id = req.params.id;
+  UserModel.findById({ _id: id }, (err, result) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    res.json(result);
+  });
+});
 
-// edit user by id 
-app.put('/user', (req, res, next) => {
-    UserModel.update({
-        _id: req.body.id
-    },{
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        password: req.body.password
-    }, (err, result) => {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        res.json(result);
-    })
-})
+// edit user by id
+app.put("/user", (req, res, next) => {
+  UserModel.update(
+    {
+      _id: req.body.id
+    },
+    {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: req.body.password
+    },
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      res.json(result);
+    }
+  );
+});
 
 ////////////////////LOGIN AUTHENTICATED
 
+app.post("/user/login", (req, res) => {
+  let userEmail = req.body.email;
+  let userPassword = req.body.password;
 
-  app.post('/user/login',(req, res) => {
-    let userEmail = req.body.email;
-    let userPassword = req.body.password;
-
-    UserModel.findOne({ email: userEmail}, (err,result) => {
-        if(err){
-            console.log(err);
-            return;
-        }
-        if(result == null){
-            res.json({
-                message: "User does not exist",
-                loggedIn: false
-            })
-            return;
-        }
-        if(userPassword == result.password){
-            res.json({
-                message: 'logged in successfuly',
-                loggedIn: true
-            })
-            return;
-        }
-        res.json({
-            message: 'Email or password incorrect',
-            loggedIn: false
-        })
-    })
+  UserModel.findOne({ email: userEmail }, (err, result) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    if (result == null) {
+      res.json({
+        message: "User does not exist",
+        loggedIn: false
+      });
+      return;
+    }
+    if (userPassword == result.password) {
+      res.json({
+        message: "logged in successfuly",
+        loggedIn: true
+      });
+      return;
+    }
+    res.json({
+      message: "Email or password incorrect",
+      loggedIn: false
+    });
   });
+});
 
+// ISSUE ROUTES
+const IssueModel = require("./models/issue.model");
 
+// Create
+// app.post('/issue', (req,res) => {
+//     IssueModel.create(req.body, (err, result) =>{
+//         if(err){
+//             console.log(err)
+//             return;
+//         }
+//         res.json(result);
+//     })
+// })
 
+app.post("/issue", (req, res) => {
+  IssueModel.create(req.body, (err, result) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    res.json(result);
+  });
+});
 
+//delete
+app.delete("/issue/:id", (req, res) => {
+  IssueModel.deleteOne({ _id: req.params.id }, (err, result) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    res.json(result);
+  });
+});
 
+// get
+app.get("/issue/", (req, res) => {
+  IssueModel.find({}, (err, result) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    res.json(result);
+  });
+});
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/test', (req, res) => res.json({
+// update
+app.put("/issue/:id", (req, res) => {
+  let obj = {};
+  if (req.body.resolved) {
+    obj.resolved = req.body.resolved;
+    obj.resolved_at = Date.now();
+  }
+  if (req.body.title) {
+    obj.title = req.body.title;
+  }
+
+  IssueModel.updateOne({ _id: req.params.id }, obj, (err, result) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    res.json(result);
+  });
+});
+
+app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/test", (req, res) =>
+  res.json({
     message: "Cohuni o njerz"
-}))
+  })
+);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
